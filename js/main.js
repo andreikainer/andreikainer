478e06977b3a43e71b5922cae5bc3ff4985d9456
Andrei Kainer <email@andreikainer.com>
2015-02-09 16:17:49 +1300

    Andrei Kainer Portfolio

---
+++ js/main.js
@@ -0,0 +1,82 @@
+//========================
+//PRELOADER
+//========================
+$(window).load(function() { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // then fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow');
+	$('body').delay(350).css({'overflow':'visible'}); // then fade out the preloder DIV that covers the website.
+})
+
+//========================
+//SMOOTHSCROLL
+//========================
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//========================
+//SLIDESHOW
+//========================
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+//========================
+//NAVBAR
+//========================
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance in pixel user needs to scroll before fadeIn
+            if ($(this).scrollTop() > 40) {
+                $('.navbar')
+                .removeClass('animated fadeOutUp')
+                .addClass('animated fadeInDown')
+                .fadeIn();
+                
+            } else {
+                $('.navbar')
+                .removeClass('animated fadeInDown')
+                .addClass('animated fadeOutUp')
+                .fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
+
+//========================
+//icon hover effect
+//========================
+$('#services img').hover(
+       function(){ $(this).addClass('animated pulse') },
+       function(){ $(this).removeClass('animated pulse') })
+
+// EOF main.js
